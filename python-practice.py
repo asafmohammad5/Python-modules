8d1da39bc7c561f4d86fc6e27681e94f9b790325
asaf <asafmohammad5@gmail.com>
2021-03-19 17:20:29 -0500

    Running some functions to get used to syntax

--- python-practice.py
+++ python-practice.py
@@ -2,3 +2,13 @@ spam = 0
 while spam < 5:
   print('Hello, world.')
   spam = spam + 1
+
+
+def greeting(name, saying="Hello"):
+    print(saying, name)
+
+def average(num1, num2):
+    return (num1/num2)
+
+
+print(average(6, 2))
