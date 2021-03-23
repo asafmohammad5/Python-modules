7d074f418247155516251d80bde2f88b369001d2
asaf <asafmohammad5@gmail.com>
2021-03-23 15:41:25 -0500

    Practicing some more pythom

--- python-practice.py
+++ python-practice.py
@@ -16,3 +16,9 @@ print(average(6, 2))
 quarters = ['First', 'Second', 'Third', 'Fourth']
 print(enumerate(quarters))
 print(enumerate(quarters, start=1))
+
+
+def find_index(elements, value):
+    index = bisect.bisect_left(elements, value)
+    if index < len(elements) and elements[index] == value:
+        return index
