c8893343aa83f564dce3ee448ab40d3d3349a75b
asaf <asafmohammad5@gmail.com>
2021-03-26 13:40:39 -0500

    Did leetcode problem 1748 (python)

--- python-practice.py
+++ python-practice.py
@@ -22,3 +22,19 @@ def find_index(elements, value):
     index = bisect.bisect_left(elements, value)
     if index < len(elements) and elements[index] == value:
         return index
+
+ public int sumOfUnique(int[] nums) {
+        int[] cnt = new int[101];
+        for (int n : nums) {
+            ++cnt[n];
+        }
+        int sum = 0;
+        for (int i = 1; i <= 100; ++i) {
+            if (cnt[i] == 1) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+
+    
