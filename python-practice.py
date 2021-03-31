4d34ec09f7ea51d8b8b70079c18cfc5299fd8a5e
asaf <asafmohammad5@gmail.com>
2021-03-31 16:07:59 -0500

    Did leetcode problem 344 (python)

--- python-practice.py
+++ python-practice.py
@@ -64,3 +64,10 @@ class Solution:
                 
         return counter
     
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+       
+        size = len(s)
+
+        for i in range(size//2):
+            s[i], s[-i-1] = s[-i-1], s[i]
