194a5e870764e5c32d5d24e49702eabda26c8f61
asaf <asafmohammad5@gmail.com>
2021-04-21 15:25:52 -0500

    Did leetcode problem 1464 using python

--- python-practice.py
+++ python-practice.py
@@ -182,3 +182,10 @@ class Solution(object):
                 count += 1
         
         return ''.join(output_S)
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        a = b = 0
+        for n in nums:
+            a, b = max(a, n), max(b, min(a, n))
+        return (a-1) * (b-1)
