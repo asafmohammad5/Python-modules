71452f9b432f5153372e8ef36f55c77e803459a9
asaf <asafmohammad5@gmail.com>
2021-04-23 14:29:05 -0500

    Did leetcode problem 728 using python

--- python-practice.py
+++ python-practice.py
@@ -189,3 +189,23 @@ class Solution:
         for n in nums:
             a, b = max(a, n), max(b, min(a, n))
         return (a-1) * (b-1)
+
+def is_self_dividing(x):
+    s = str(x)
+    for d in s:
+        if d=="0" or x%int(d)!=0:
+            return False
+    return True
+
+class Solution(object):
+    def selfDividingNumbers(self, left, right):
+        """
+        :type left: int
+        :type right: int
+        :rtype: List[int]
+        """
+        ans = []
+        for x in xrange(left, right+1):
+            if is_self_dividing(x):
+                ans.append(x)
+        return ans
