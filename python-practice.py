954a8b3138713e47b9b162a669dec1e2d1f49bcb
asaf <asafmohammad5@gmail.com>
2021-05-04 18:47:55 -0400

    Did leetcode problem 1299

--- python-practice.py
+++ python-practice.py
@@ -247,3 +247,13 @@ class Solution:
             seen.add(num)
         
         return sum(seen.difference(duplicates))
+
+class Solution(object):
+    def replaceElements(self, arr):
+    
+        me,arr[-1] = arr[-1],-1
+        
+        for i in range(len(arr)-2,-1,-1):
+            arr[i],me = me,max(me,arr[i])
+            
+        return arr
